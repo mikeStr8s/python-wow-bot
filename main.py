cb34c9c0545521399913ee1972fdbff785e3d2a5
MikeStr8s <stratinm@mail.gvsu.edu>
2018-06-12 20:28:44 -0400

    New screengrab technique

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 import numpy as np
 import cv2
 import imutils
-import pyautogui
 import pytesseract
-from PIL import Image
+from PIL import Image, ImageGrab
 
 
 def main():
-    # Get the players name, current health, and current resource value
-    while True:
-        health, resource = get_bar_values()
-        print(health, resource)
+    get_bar_values()
+    # # Get the players name, current health, and current resource value
+    # while True:
+    #     health, resource = get_bar_values()
+    #     print(health, resource)
 
 
 def get_bar_values():
@@ -21,11 +21,12 @@ def get_bar_values():
     :return: health and resource list values
     """
     # Grab the screenshot
-    pre_image = cv2.imread('wow_screenshot.jpg', 0)
-    # Extract the entire character bar
-    character_bar = pre_image[20:125, 25:300]
-    coords = [character_bar[44:59, 100:266], character_bar[59:75, 100:266]]
+    game_window = np.array(ImageGrab.grab(bbox=(0, 100, 1600, 1280)))
+    game_window = cv2.cvtColor(game_window, cv2.COLOR_BGR2GRAY)
+    character_bar = game_window[25:130, 35:315]
+    coords = [character_bar[32:50, 120:256], character_bar[50:68, 120:256]]
 
+    # Extract the entire character bar
     def get_value(ui):
         """
         This internal function is for getting bar values from screen coordinates
@@ -33,21 +34,18 @@ def get_bar_values():
         :return: string value found in the screenshot region
         """
         # Double the size of the image
-        ui = cv2.resize(ui, (0, 0), fx=2, fy=2)
+        ui = cv2.resize(ui, (0, 0), fx=3, fy=3)
         # Create threshold of grayscale image. Anything from value 0-130 is white, > 130 is black
-        ui = cv2.inRange(ui, 0, 130)
+        ui = cv2.inRange(ui, 0, 145)
+        ui = cv2.medianBlur(ui, 3)
+        cv2.imshow('win', ui)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
         # OCR on the threshold image
         return pytesseract.image_to_string(Image.fromarray(ui))
 
     # Return lists of parsed health and resource values
     return [int(x) for x in get_value(coords[0]).split('/ ')], [int(x) for x in get_value(coords[1]).split('/ ')]
-
-
-def take_screenshot():
-    """
-    This function takes a screenshot of the main monitor
-    """
-    pyautogui.screenshot('wow_bot_screengrab.png')
 
 
 if __name__ == "__main__":
